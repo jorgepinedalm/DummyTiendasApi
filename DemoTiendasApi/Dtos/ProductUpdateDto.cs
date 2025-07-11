464c8dced683233ac439e4a7170595959e452dc2
jorgepinedalm <jorge.pineda@onoff.com.co>
2025-07-11 08:04:11 -0500

    feat: change sales tax values in mock products

--- DemoTiendasApi/Dtos/ProductUpdateDto.cs
+++ DemoTiendasApi/Dtos/ProductUpdateDto.cs
@@ -6,7 +6,7 @@
         public int PurchaseTaxId { get; set; }
         public int SalesTaxId { get; set; }
         public int PresentationId { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public string? Description { get; set; }
         public decimal SalesPrice { get; set; }
         public decimal NetContent { get; set; }
